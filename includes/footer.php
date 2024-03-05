569269d664a9c35942dc61a4295be9dce68954ce
pursottam1234 <purush.bhandari69@gmail.com>
2024-03-05 09:54:57 +0545

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Style Studio | All Rights Reserved | Contact Us: +977 90000 00000</b></span></div>
+</footer>
+    
